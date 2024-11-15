c19c7d396b5121c75e779fc63c7d084adc307636
pdrzan <pedeozanelato19@gmail.com>
2024-11-15 16:46:11 -0400

    feat: add instances

---
+++ instances/t2ex.sol
@@ -0,0 +1,44 @@
+Problem:    
+Rows:       5
+Columns:    7
+Non-zeros:  14
+Status:     OPTIMAL
+Objective:  obj = 24 (MINimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 cons_s       NS             1             1             =            18 
+     2 cons_0       NS             0             0             =            -5 
+     3 cons_2       B              0             0             = 
+     4 cons_3       NS             0             0             =             8 
+     5 cons_t       NS            -1            -1             =            -6 
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 x01          NL             0             0             1             3 
+     2 x13          NL             0             0             1            21 
+     3 x20          B              0             0             1 
+     4 x21          B              1             0             1 
+     5 x32          B              1             0             1 
+     6 x43          B              1             0             1 
+     7 x24          NL             0             0             1            18 
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 0.00e+00 on column 0
+        max.rel.err = 0.00e+00 on column 0
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
